5f39c083dec58a81974c1591f9489722bc127f19
NastyaSkv <NastyaSkv@DESKTOP-S0J6MVO>
2022-10-21 22:36:31 +0300

    done

--- Introduction ToOOP/main.cpp
+++ Introduction ToOOP/main.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 #define tab "\t"
+#define delimiter "\n----------------------------------------------------\n"
 
 /*struct Point
 {
@@ -32,35 +33,53 @@ public:
 	}
 
 	//      CONSTRUCTORS:
-	Point()
+	/*Point() //default
 	{
 		x = y = double();   //double()-значение по умолчанию для типа double
 		cout << "Constructor:\t" << this << endl;
-	}
-	Point(double x)
+	}*/
+	/*Point(double x)
 	{
 		this->x = x;
 		this->y = 0;
 		cout << "1ArgConstructor:" << this << endl;
-	}
-	Point(double x, double y)
+	}*/
+	Point(double x = 0, double y = 0) //универсальный конструктор
 	{
 		this->x = x;
 		this->y = y;
 		cout << "Constructor:\t" << this << endl;
 	}
+	Point(const Point& other)  //Copy Connstructor
+	{
+		this->x = other.x;
+		this->y = other.y;
+		cout << "Copy Constructor: " << this << endl;
+	}
+
 	~Point()
 	{
 		cout << "Destructor:\t" << this << endl;
 	}
 
-	//      METHODS:
-	double distance(Point other)
+	//            OPERATORS    //Оператор присваивания
+	Point& operator=(const Point& other)
 	{
-		double x_distance = this->x - other.x;
+		this->x = other.x;
+		this->y = other.y;
+		cout << "CopyAssignment:\t" << this << endl;
+		return *this;
+	}
+
+	//      METHODS:
+	double distance(const Point& other)const //const - гарантия безопасности
+	{
+		/*double x_distance = this->x - other.x;
 		double y_distance = this->y - other.y;
 		double distance = sqrt(x_distance * x_distance + y_distance * y_distance);
-		return distance;
+		return distance;*/
+		//оптимизируем:
+		return sqrt(pow(this->x - other.x, 2) + pow(this->y - other.y, 2));
 	}
 	void print()const
 	{
@@ -68,22 +87,24 @@ public:
 	}
 };
 
-double distance(Point A, Point B)
+double distance(const Point& A, const Point& B)
 {
-	double x_distance = A.get_x() - B.get_x();
+	/*double x_distance = A.get_x() - B.get_x();
 	double y_distance = B.get_x() - B.get_x();
 	double distance = sqrt(x_distance*x_distance+y_distance*y_distance);
-	return distance;
+	return distance;*/
+	//оптимизируем:
+	return sqrt(pow(A.get_x() - B.get_x(), 2) + pow(A.get_y() - B.get_y(), 2));
 }
 
 //#define STRUCT_POINT
 //#define GET_SET_CHECK
-#define DISTANCE_CHECK
+//#define DISTANCE_CHECK
 //#define CONSTRUCTORS_CHECK
 
 void main()
 {
-	setlocale(LC_ALL, " ");
+	setlocale(LC_ALL, "Russian");
 	//cout << "Hello OOP" << endl;
 
 #ifdef STRUCT_POINT
@@ -113,13 +134,20 @@ void main()
 	B.print();
 #endif
 
-	#ifdef DISTANCE_CHECK
+#ifdef DISTANCE_CHECK
 	Point A(2, 3);
 	Point B(5, 7);
 	A.print();
 	B.print();
+	cout << delimiter << endl;
 	cout << "Расстояние от точки А до точки В: " << A.distance(B) << endl;
+	cout << delimiter << endl;
 	cout << "Расстояние от точки B до точки A: " << B.distance(A) << endl;
+	cout << delimiter << endl;
+	cout << "Расстояние от точки А до точки В: " << distance(A, B) << endl;
+	cout << delimiter << endl;
+	cout << "Расстояние от точки B до точки A: " << distance(B, A) << endl;
+	cout << delimiter << endl;
 #endif
 
 #ifdef CONSTRUCTORS_CHECK
@@ -131,5 +159,18 @@ void main()
 
 	Point C(2, 3);
 	C.print();
-#endif//Homework 2
+
+	Point D = C;  //Copy Constructor
+	D.print();    //здесь к-р отработал неявно
+	Point E;
+	E = D;
+	E.print();
+#endif
+
+	int  a, b, c;
+	a = b = c = 0;
+	Point A, B, C;
+	cout << delimiter << endl;
+	A = B = C = Point(2, 3);
+	cout << delimiter << endl;
 }
