22691834522492f8060399bdfb846a566f880342
NastyaSkv <NastyaSkv@DESKTOP-S0J6MVO>
2022-10-22 21:25:27 +0300

    homework

--- Homework 2/main.cpp
+++ Homework 2/main.cpp
@@ -1,18 +1,13 @@
-﻿/*//1. Написать метод ? ? ? distance(? ? ? ) который возвращает расстояние до указанной точки;
+﻿//1. Написать метод ? ? ? distance(? ? ? ) который возвращает расстояние до указанной точки;
 //2. Написать функцию ? ? ? distance(? ? ? ) которая возвращает расстояние между двумя точками;
 #include<iostream>
-#include<cmath>
 using namespace std;
-
-double distance(double x, double y, double c, double d);
 
 class Point
 {
 	double x;
 	double y;
-	double c;
-	double d;
-	double z;
+
 public:
 
 	double get_x()const
@@ -38,36 +33,25 @@ public:
 		this->y = y;
 	}
 	~Point() {}
-	/*
-	void Print()const
-	{
-		cout << c << ";" << d << endl;
-	}
-	void Print_xy(double x, double y)
-	{
-		cout << x << ";" << y << endl;
-	}
 	
-	double Distance(Point other)
+	double Distance(const Point& other) const //Method
 	{
-		return(sqrt(pow(this->x - other.x, 2) + (pow(this->y - other.y, 2));
+		return sqrt(pow(this->x - other.x, 2) + (pow(this->y - other.y, 2)));
 	}
 };
+
+double distance(const Point& A, const Point& B) //Function
+{
+	return sqrt(pow(A.get_x() - B.get_x(), 2) + (pow(A.get_y() - B.get_y(), 2)));
+}
 
 void main()
 {
 	setlocale(LC_ALL, "");
 	Point A(2, 3);
-
 	Point B(8, 9);
 
-	cout << "Расстояние до точки = " << B.Distance(A) << endl;
-	//cout << "Расстояние между двумя точками = " << distance(X, Y, C, D) << endl;
+	cout << "Расстояние до точки = " << A.Distance(B) << endl;              //Method
+	cout << "Расстояние между двумя точками = " << distance(A, B) << endl;  //Function
 }
 
-double distance(double x, double y, double c, double d)
-{
-	double z = sqrt((x - c) * (x - c) + (y - d) * (y - d));
-	return(z);
-}
-*/
