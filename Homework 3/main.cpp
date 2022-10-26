f754b61a935e127dca3a07a488a93f4da8521ec5
NastyaSkv <NastyaSkv@DESKTOP-S0J6MVO>
2022-10-26 15:33:02 +0300

    Homework

--- Homework 3/main.cpp
+++ Homework 3/main.cpp
@@ -1,8 +1,16 @@
-﻿//Реализовать класс Fraction, описывающий простую дробь.
+﻿//ДЗ 3: Реализовать класс Fraction, описывающий простую дробь.
 //В классе должны быть все необходимые методы, так же нужно обеспечить
 //выполнение арифметических и других важных операций над дробями;
+
+//ДЗ 4: Для класса Fraction перегрузить:
+//1. Составные присваивания : +=, -=, *=, /=;
+//2. Операторы сравнения : == , != , > , < , >= , <= ;
+//3. Написать метод reduce(), который сокращает дробь;
+
 #include<iostream>
 using namespace std;
+
+#define delimiter "\n----------------------------------------------------\n"
 
 class Fraction
 {
@@ -33,10 +41,11 @@ public:
 	}
 	void set_o(int o)
 	{
+		if (o == 0)o = 1; //фильтрация деления на НОЛЬ
 		this->o = o;
 	}
 
-	//CONSTRUCTOR
+	//			  CONSTRUCTOR
 	Fraction(int x = 0, int y = 0, int o = 0)
 	{
 		this->x = x;
@@ -47,6 +56,7 @@ public:
 	{
 		//cout << "DESTRUCTOR" << endl;
 	}
+
 	//            OPERATORS              
 	Fraction& operator=(const Fraction& other)
 	{
@@ -55,20 +65,39 @@ public:
 		this->o = other.o;
 		return *this;
 	}
-
-	//METHODS
-	Fraction Unified(const Fraction& other)const  //приведение к общему знаменателю
+	Fraction& operator+= (const Fraction& other)
 	{
-		Fraction C, D, rezz;
-		C.y = this->y * other.y;           // C = A
-		C.x = C.y / this->y * this->x;
-		D.y = C.y;                        // D = B
-		D.x = D.y / other.y * other.x;
-		rezz.x = C.x + D.x;
-		rezz.y = C.y;
-		return rezz;
+		this->x = this->x + other.x;
+		this->y = other.y;
+		return *this;
+	}
+	Fraction& operator-= (const Fraction& other)
+	{
+		this->x = this->x - other.x;
+		this->y = other.y;
+		return *this;
+	}
+	Fraction& operator*= (const Fraction& other)
+	{
+		this->x = this->x * other.x;
+		this->y = this->y * other.y;
+		return *this;
+	}
+	Fraction& operator/= (const Fraction& other)
+	{
+		this->x = this->x * other.y;
+		this->y = this->y * other.x;
+		return *this;
 	}
 
+	//            METHODS
+	void Unified(Fraction& other) //приведение к общему знаменателю
+	{
+		this->x = this->y * other.y / this->y * this->x;
+		other.x = this->y * other.y / other.y * other.x;
+		this->y = this->y * other.y;
+		other.y = this->y;
+	}
 	Fraction Reduction() //сокращение дробей
 	{
 		for (int i = 10; i > 1; i--)
@@ -81,7 +110,6 @@ public:
 			}
 		} return *this;
 	}
-
 	Fraction Integer()  //выделение общей части
 	{
 		if (this->x > this->y)
@@ -91,33 +119,20 @@ public:
 		}
 		return *this;
 	}
-
 	Fraction Sum(const Fraction& other)const   //Сумма
 	{
 		Fraction res;
-		if (this->y != other.y)return Unified(other);
-		else
-		{
-			res.x = this->x + other.x;
-			res.y = this->y;
-			return res;
-		}
+		res.x = this->x + other.x;
+		res.y = this->y;
+		return res;
 	}
-
 	Fraction Difference(const Fraction& other)const   //Разность
 	{
 		Fraction res;
-		res.x = -other.x;
-		res.y = other.y;
-		if (this->y != other.y)return Unified(res);
-		else
-		{
-			res.x = this->x - other.x;
-			res.y = this->y;
-			return res;
-		}
+		res.x = this->x - other.x;
+		res.y = this->y;
+		return res;
 	}
-
 	Fraction Multiplication(const Fraction& other)const //умножение
 	{
 		Fraction res;
@@ -125,7 +140,6 @@ public:
 		res.y = this->y * other.y;
 		return res;
 	}
-	
 	Fraction Division(const Fraction& other)const //деление
 	{
 		Fraction res;
@@ -156,6 +170,30 @@ ostream& operator<<(ostream& os, const Fraction& obj)
 	}
 	return os;
 }
+bool operator==(const Fraction& left, const Fraction& right)
+{
+	return left.get_x() == right.get_x() && left.get_y() == right.get_y();
+}
+bool operator!=(const Fraction& left, const Fraction& right)
+{
+	return left.get_x() != right.get_x() || left.get_y() != right.get_y();
+}
+bool operator>(const Fraction& left, const Fraction& right)
+{
+	return left.get_x() > right.get_x() && left.get_y() == right.get_y();
+}
+bool operator<(const Fraction& left, const Fraction& right)
+{
+	return left.get_x() < right.get_x() && left.get_y() == right.get_y();
+}
+bool operator>=(const Fraction& left, const Fraction& right)
+{
+	return left.get_x() >= right.get_x() && left.get_y() == right.get_y();
+}
+bool operator<=(const Fraction& left, const Fraction& right)
+{
+	return left.get_x() <= right.get_x() && left.get_y() == right.get_y();
+}
 
 void main()
 {
@@ -170,27 +208,69 @@ void main()
 	Fraction B(r, t);
 	cout << "Дробь В: " << B << endl << endl;
 
-	Fraction C;
-	C = A.Sum(B);                         //считаем сумму
-	C.Reduction();                        //сокращаем
-	C.Integer();                          //выделяем целую часть
-	cout << "Сумма дробей: A + B = " << C << endl;
+	Fraction C, D;
+	C = A; D = B;
+	if (C.get_y() != D.get_y())C.Unified(D);
 
-	Fraction P;
-	P = A.Difference(B);                  //считаем разность
-	P.Reduction();                        //сокращаем
-	P.Integer();                          //выделяем целую часть
-	cout << "Разность дробей: A - B = " << P << endl;
+	/*  //HOMEWORK 3
+		Fraction N;
+		N = A.Sum(B);                         //считаем сумму
+		N.Reduction();                        //сокращаем
+		N.Integer();                          //выделяем целую часть
+		cout << "Сумма дробей: A + B = " << N << endl;
 
-	Fraction E;
-	E = A.Multiplication(B);              //умножаем
-	E.Reduction();                        //сокращаем
-	E.Integer();                          //выделяем целую часть
-	cout << "Умножение дробей: A * B = " << E << endl;
+		Fraction P;
+		P = A.Difference(B);                  //считаем разность
+		P.Reduction();                        //сокращаем
+		P.Integer();                          //выделяем целую часть
+		cout << "Разность дробей: A - B = " << P << endl;
 
-	Fraction W;
-	W = A.Division(B);                    //делим
-	W.Reduction();                        //сокращаем
-	W.Integer();                          //выделяем целую часть
-	cout << "Деление дробей: A : B = " << W << endl;
+		Fraction E;
+		E = A.Multiplication(B);              //умножаем
+		E.Reduction();                        //сокращаем
+		E.Integer();                          //выделяем целую часть
+		cout << "Умножение дробей: A * B = " << E << endl;
+
+		Fraction W;
+		W = A.Division(B);                    //делим
+		W.Reduction();                        //сокращаем
+		W.Integer();                          //выделяем целую часть
+		cout << "Деление дробей: A : B = " << W << endl<<endl;
+	*/
+	//HOMEWORK 4
+	if (C == D)cout << "Дроби равны" << endl;
+	else if (C != D)cout << "Дроби НЕ равны" << endl;
+	if (C > D)cout << A << " > " << B << endl;
+	else if (C < D)cout << A << " < " << B << endl;
+	if (C >= D)cout << A << " >= " << B << endl;
+	else if (C <= D)cout << A << " <= " << B << endl << endl;
+
+	cout << delimiter << endl;
+	cout << "A = " << C << "\t\t" << "B = " << D << endl << endl;
+	Fraction redB = D;
+	redB.Reduction();
+
+	Fraction sum = C;       //СУММА
+	sum += D;
+	cout << "A += B  |->  " << "A = " << sum << "\t";
+	sum.Reduction();
+	cout << "   |->  " << "A = " << sum << "\t" << "B = " << redB << endl;
+	
+	Fraction dif = C;      //РАЗНОСТЬ
+	dif -= D;
+	cout << "A -= B  |->  " << "A = " << dif << "\t";
+	dif.Reduction();
+	cout << "   |->  " << "A = " << dif << "\t" << "B = " << redB << endl;
+	
+	Fraction mult = C;     //УМНОЖЕНИЕ
+	mult *= D;
+	cout << "A *= B  |->  " << "A = " << mult << "\t";
+	mult.Reduction();
+	cout << "   |->  " << "A = " << mult << "\t" << "B = " << redB << endl;
+	
+	Fraction div = C;      //ДЕЛЕНИЕ
+	div /= D;
+	cout << "A /= B  |->  " << "A = " << div << "\t";
+	div.Reduction();
+	cout << "   |->  " << "A = " << div << "\t" << "B = " << redB << endl;
 }
